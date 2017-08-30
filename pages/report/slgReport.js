1a0c91a4dc037e8af56e73f07329803fa832ecd2
AshishKumar <ashish_kumar2@homedepot.com>
2017-08-30 15:43:58 +0530

    Made changes to the MobileReport.html

--- pages/report/slgReport.js
+++ pages/report/slgReport.js
@@ -7,7 +7,9 @@
     var startRowNo = 1;
     var endRowNo = 10;
 
+    /** initialize dropdown for MaterializeCSS */
     $('select').material_select();
+    $('.tooltipped').tooltip({delay: 50});
 
     $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
@@ -18,15 +20,126 @@
     closeOnSelect: true // Close upon selecting a date,
   });
 
+    function isEmptyOrBlank(checkObject)
+    {
+        if(undefined != checkObject && null != checkObject)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    loadLeads();
+
+    function loadLeads() 
+    {
+        $.get('data/SLG_Mock.json', function(data) 
+        {
+            console.log("data:: "+ data );
+            console.log("message is::"+ data.response[0]);
+            if(undefined != data && null != data && undefined != data.response && null != data.response && data.response.length > 0)
+            {
+                var lead;
+                var title, listPrice, storeQty, dcQty, storeNum, tbRow, partDesc;
+                if(data.response.length < noOfRowsDisplayed)
+                {
+                    noOfRowsDisplayed = data.response.length;
+                    startRowNo = 1;
+                    endRowNo = noOfRowsDisplayed;
+                }
+                else
+                {
+                    startRowNo = 1;
+                    endRowNo = noOfRowsDisplayed;
+                }
+                for (var i = 0; i < data.response.length; i ++)
+                {
+                    if(i == endRowNo)
+                    {
+                        // last recored reached. Stop.
+                        $('#paginationText').text(startRowNo + "-" + endRowNo + " of " + data.response.length);
+                        break;
+                    }
+                    lead = data.response[i];
+                    title = "";
+                    listPrice = "";
+                    storeQty = "";
+                    dcQty = "";
+                    storeNum = "";
+                    if(isEmptyOrBlank(lead.PartNumber) && $.trim(lead.PartNumber) != "")
+                    {
+                        title = $.trim(lead.PartNumber);
+                    }
+                    else
+                    {
+                        title = $.trim(lead.SearchedKeyword);
+                    }
+
+
+                    if(!isEmptyOrBlank(lead.ListPrice))
+                    {
+                        listPrice = $.trim(lead.ListPrice);
+                    }
+                    if(!isEmptyOrBlank(lead.Partdescription))
+                    {
+                        partDesc = $.trim(lead.Partdescription);
+                    }
+                    if(!isEmptyOrBlank(lead.Store_Quantity))
+                    {
+                        storeQty = $.trim(lead.Store_Quantity);
+                    }
+                    if(!isEmptyOrBlank(lead.DC_Quantity))
+                    {
+                        dcQty = $.trim(lead.DC_Quantity);
+                    }
+                    if(!isEmptyOrBlank(lead.NapaStoreNumber))
+                    {
+                        storeNum = $.trim(lead.NapaStoreNumber);
+                    }
+
+                    tbRow = '<li class="collection-item avatar">' +
+                            '<img src="images/header_logo.png" alt="" class="circle">';
+                    if($.trim(partDesc) == "")
+                    {
+                        tbRow = tbRow + '<span class="title"><strong>'+ title +'</strong></span>'; 
+                    }
+                    else
+                    {
+                        tbRow = tbRow + '<span class="title tooltipped" data-position="top" data-delay="50" data-tooltip="'+ partDesc +'"><strong>'+ title +'</strong><i class="tiny material-icons napaBlue">info</i></span>'; 
+                    }
+                    tbRow = tbRow +     
+                            '<p>' +
+                                'List Price: $' +listPrice+ ' <br> ' +
+                                'Quantity - Store: ' + storeQty + ' | Dc: ' + dcQty + '<br>' +
+                                'Store #: ' + storeNum +
+                                
+                            '</p>' +
+                            '<a class=" secondary-content btn-floating btn-small waves-effect waves-light napaActionButton">' +
+                                '<i class="small material-icons">mode_edit</i>' +
+                            '</a>' +
+                        '</li>'; 
+                        $('ul#mobileTableContents').append(tbRow);
+                        $('.tooltipped').tooltip({delay: 50});
+
+                }
+            }
+        });
+    }
+
+
+
   $('#rowsPerPageSelect').change(function() {
+
     var requestedNoOfRows = $(this).val();
-    noOfRowsDisplayed = $('#mobileTable ul li').length;
+    noOfRowsDisplayed = $('ul#mobileTableContents li').length;
+    var tableRow;
     if(requestedNoOfRows < noOfRowsDisplayed)
     {
         var i = noOfRowsDisplayed - requestedNoOfRows;
-        while (i > 1)
+        while (i > 0)
         {
-            $('#mobileTable ul li:last-child').remove();
+            tableRow = $('ul#mobileTableContents li:last-child');
+            $('ul#mobileTableContents li:last-child').remove();
             i--;
         }
     }
@@ -36,11 +149,6 @@
     }
      
 });
-
-// $('.datepicker').pickadate({
-//     selectMonths: true, // Creates a dropdown to control month
-//     selectYears: 15 // Creates a dropdown of 15 years to control year
-//   });
 
 //   $('.datepicker').pickadate({
 //       selectMonths: true, // Creates a dropdown to control month
